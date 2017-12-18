99022f8bf0566521bb1c441d30cf5c48faac68f7
JayloneZ <andrezyczkowski@gmail.com>
2017-12-18 16:58:07 +0000

    CRUD operations work

--- controllers/films.js
+++ controllers/films.js
@@ -2,4 +2,31 @@
 const Film = require('../client/src/models/film');
 const Review = require('../client/src/models/review');
 const getFilms = require('../client/src/models/films');
+const express = require('express');
+const filmRouter = new express.Router();
 const films = getFilms();
+
+filmRouter.get('/', function (req, res) {
+	res.json(films);
+});
+
+filmRouter.post('/', function (req, res) {
+	const newFilm = req.body.film;
+	films.push(newFilm);
+	res.json(films);
+});
+
+// see one film
+filmRouter.get('/:id', function (req, res) {
+	const selectedFilm = films[req.params.id];
+	res.json(selectedFilm);
+});
+
+// edit a film
+filmRouter.put('/:id', function (req, res) {
+	const newFilm = req.body.film;
+	films[req.params.id] = newFilm;
+	res.json(films);
+});
+
+module.exports = filmRouter;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('client/public'));
+app.use(require('./controllers/index.js'));
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(`${__dirname}/client/public/index.html`);
+// });
 
 app.listen(3000, function () {
   console.log(`Films app listening on port ${ this.address().port }`);
